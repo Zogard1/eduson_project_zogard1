d926b7c35d9ea65cff549874c97730dd0d547615
Zogard1 <fix12max456@gmail.com>
2025-04-27 17:04:05 +0300

    1231414

---
+++ eduson_project.py
@@ -0,0 +1,4 @@
+if True:
+   print('Hi, there')
+if __name__ == '__main__':
+    print('i am inside main')
---
+++ eduson_project1.py
@@ -0,0 +1,11 @@
+MY_CONSTANT = 'Этот результат'
+
+
+def this_is_function():
+    first_variable = 2
+    second_variable = 4
+    third_variable = first_variable + second_variable
+    return MY_CONSTANT, third_variable
+
+
+print(this_is_function())
